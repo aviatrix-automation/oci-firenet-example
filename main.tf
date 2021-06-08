c341e71e9ac8672a1ff6e7d509aa91a6b06f07ee
Travis Mitchell <trvsmtchll@yahoo.com>
2021-06-08 14:43:33 -0400

    Update demo for blog
    - updated module version number for OCI blog

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# OCI Transit FireNet Module
+# OCI Transit FireNet Module Demo
 module "oci_transit_firenet_1" {
   source      = "terraform-aviatrix-modules/oci-transit-firenet/aviatrix"
-  version     = "4.0.1"
+  version     = "4.0.3"
   cidr        = "10.10.0.0/16"
   region      = "us-ashburn-1"
-  account     = "TM-OCI" 
+  account     = "TM-OCI" # Replace with the name of your OCI Access Account in Aviatrix Controller
 }
